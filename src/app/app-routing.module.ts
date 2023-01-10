1cbdba48f6cfce65309db6c093a7a51be806acef
Chanelp <mariannissanchez27@gmail.com>
2023-01-09 20:43:07 -0400

    Precarga de modulos: Custom Strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 import { NotFoundComponent } from './website/pages/not-found/not-found.component';
+import { CustomPreloadService } from './services/custom-preload.service';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule)
+    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule),
+    data: {
+      preload: true
+    }
   },
   {
     //Cargar modulos, esto es lo que nos habilita el lazy loading y code splitting
@@ -20,7 +24,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules
+    preloadingStrategy: CustomPreloadService
   })],
   exports: [RouterModule],
 })
